bfd33caafc22db6086344a61fa241da5bc5a9d0c
Johand <arijohand25@gmail.com>
2025-07-25 13:57:57 +0700

    update file db.js to support railways config

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,3 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
-
-// module.exports = pool;
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
